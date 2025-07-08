44141d6f85711fb1b442d7751931ecaf8ebf7a0b
Rohan Chinchkar <chinchkarrohan@gmail.com>
2025-07-08 20:23:07 +0530

    initial commit

---
+++ src/main/java/com/rohan/chinchkar/place_online_orders/PlaceOnlineOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.rohan.chinchkar.place_online_orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceOnlineOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceOnlineOrdersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rohan/chinchkar/place_online_orders/PlaceOnlineOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohan.chinchkar.place_online_orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceOnlineOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
